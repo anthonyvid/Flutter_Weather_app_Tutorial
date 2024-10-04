2e07a5e8d069885df8a81d86449bf794f8f82a3e
Anthony Vidovic <anthonyvidovic@gmail.com>
2024-10-03 20:09:45 -0400

    flutter tutorial

---
+++ lib/Models/weather_model.dart
@@ -0,0 +1,19 @@
+class Weather {
+  final String cityName;
+  final double temperature;
+  final String mainCondition;
+
+  Weather({
+    required this.cityName,
+    required this.temperature,
+    required this.mainCondition,
+  });
+
+  factory Weather.fromJson(Map<String, dynamic> json) {
+    return Weather(
+      cityName: json['name'], 
+      temperature:  json['temperature'], 
+      mainCondition:  json['mainCondition']
+    );
+  }
+}
---
+++ lib/Pages/weather_page.dart
@@ -0,0 +1,54 @@
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_application_2/Models/weather_model.dart';
+import 'package:flutter_application_2/services/weather_service.dart';
+
+class WeatherPage extends StatefulWidget {
+  const WeatherPage({super.key});
+
+  @override
+  State<WeatherPage> createState() => _WeatherPageState();
+}
+
+class _WeatherPageState extends State<WeatherPage> {
+  final _weatherService = WeatherService("21fb23edd797fe39ef10fa157bb1a9db");
+
+  Weather? _weather;
+
+  _fetchWeather() async {
+    String cityName = await _weatherService.getCurrentCity();
+
+    try {
+      final weather = await _weatherService.getWeather(cityName);
+      setState(() {
+        _weather = weather;
+      });
+    } catch (e) {
+      if (kDebugMode) {
+        print(e);
+      }
+    }
+  }
+
+  @override
+  void initState() {
+    super.initState();
+
+    _fetchWeather();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(_weather?.cityName ?? "Loading City..."),
+            Text('${_weather?.temperature.round()}°C')
+          ],
+        ),
+      ),
+    );
+  }
+}
